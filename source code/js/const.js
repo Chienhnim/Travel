bb4ac1bf29baec8e2ab151b695c33c34ad46601e
Chienhnim <99858478+Chienhnim@users.noreply.github.com>
2022-05-05 23:24:43 +0700

    fix header footer

---
+++ source code/js/const.js
@@ -0,0 +1,124 @@
+const headerHTML=`
+<div class="sys_frame header">
+        <div class="top">
+          <div class="top_content">
+            <div class="phone">
+              <div class="icon"><i class="fa-solid fa-phone"></i></div>
+              <div class="text">888-750-868</div>
+            </div>
+            <div class="contact">
+              <div class="text">Follow Us:</div>
+              <div class="link">
+                <a class="icon facebook"><i class="fa-brands fa-facebook-f"></i></a>
+                <a class="icon twitter" ><i class="fa-brands fa-twitter"></i></a>
+                <a class="icon instagram"><i class="fa-brands fa-youtube"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="menu_main">
+          <a class="menu_open"><i class="fa-solid fa-bars"></i></a>
+
+          <div class="menu_content">
+            <a href="#" class="logo" style="background-image:url(system/icon/logo.svg);"></a>
+            <div class="menu_area">
+              <a class="item" href="">HOME</a>
+              <a class="item" href="">ABOUT US</a>
+              <a class="item" href="">FLIGHT</a>
+              <div class="item has_children">
+                <div class="parent">
+                  <div class="text">TOUR</div>
+                  <div class="icon" style="background-image:url(system/icon/white_arrow_down.svg);"></div>
+                </div>
+                <div class="list">
+                  <a class="children">Menu con 1</a>
+                  <a class="children">Menu con 1</a>
+                  <a class="children">Menu con 1</a>
+                  <a class="children">Menu con 1</a>
+                </div>
+              </div>
+              <a class="item" href="index.html">CONTACT</a>
+              <a class="item_more" href="">SHOPPING CARD</a>
+              <a class="item_more" href="">LOGIN</a>
+
+            </div>
+            <div class="menu_icon">
+              <a href="#" class="icon search"><i class="fa-solid fa-magnifying-glass"></i></a>
+              <a href="#" class="icon login"><i class="fa-solid fa-arrow-right-to-bracket"></i></a>
+            </div>
+          </div>
+        </div>
+    </div>
+    `
+
+const footerHTML=`
+<div class="sys_frame footer">
+      <div class="sys_limit footer_content">
+
+        <div class="area area1 left">
+          <a class="logo" href="index.html"  style="background-image:url(system/icon/white_logo.svg);"></a>
+          <div class="slogan">Welcome to our Trip and Tour Agency</div>
+          <div class="phone">
+            <div class="icon" style="background-image:url(system/icon/white_phone.svg);"></div>
+            <div class="text">888-750-868</div>
+          </div>
+          <div class="email">
+            <div class="icon" style="background-image:url(system/icon/white_email.svg);"></div>
+            <div class="text">gotourtravel@gmail.com</div>
+          </div>
+          <div class="address">
+            <div class="icon" style="background-image:url(system/icon/white_address.svg);"></div>
+            <div class="text">307- Nguyen Van Dau Street, Binh Thanh District, Ho Chi Minh City</div>
+          </div>
+        </div>
+
+        <div class="right">
+          <div class="area area2">
+            <div class="title">
+              <div class="text">Company</div>
+              <div class="icon" style="background-image:url(system/icon/white_add.svg);"></div>
+            </div>
+            <div class="list">
+              <a href="about_us.html">About Us</a>
+              <a href="rewards.html">Rewards</a>
+              <a href="news.html">News</a>
+              <a href="contract.html">Contract</a>
+            </div>
+          </div>
+
+          <div class="area area3">
+            <div class="title">
+              <div class="text">Explore</div>
+              <div class="icon" style="background-image:url(system/icon/white_add.svg);"></div>
+            </div>
+            <div class="list">
+              <a href="about_us.html">Account</a>
+              <a href="about_us.html">Privacy Policy</a>
+              <a href="about_us.html">Careers</a>
+              <a href="faqs.html">FAQs</a>
+              <a href="events.html">Events</a>
+            </div>
+          </div>
+
+          <div class="area area4">
+            <div class="title">
+              <div class="text">Newsletter</div>
+              <div class="icon" style="background-image:url(system/icon/white_add.svg);"></div>
+            </div>
+            <div class="list">
+              <input class="input" type="text" placeholder="Email address">
+              <div class="button">SUBSCRIBE</div>
+              <div class="policies">I agree to all terms and policies</div>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="coppy_right">
+          &copy; Coppyright 2021 by Gaviasthemes
+        </div>
+
+      </div>
+    </div>
+    `
